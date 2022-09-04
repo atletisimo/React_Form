e7ed11a4b002af573cab35d9cec3f8661941e0b9
atletisimo <37110896+atletisimo@users.noreply.github.com>
2022-09-04 14:54:30 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { useState } from "react";
 //import styled from "styled-components";
 import "./App.css";
-// import Temperature from "./components/Temperature";
+
 import Form from "./components/Form";
 const App = () => {
   return (
     <div>
       <Form />
-      {/* <Temperature /> */}
+   
     </div>
   );
 };
